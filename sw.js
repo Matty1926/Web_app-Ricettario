3b4c44b7a1d8a052387b28387b835788b02a4a50
Mattia_P <74492416+Matty1926@users.noreply.github.com>
2026-04-10 20:13:12 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'ricettario-v2';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;1,400&family=Lora:ital,wght@0,400;0,500;1,400&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
